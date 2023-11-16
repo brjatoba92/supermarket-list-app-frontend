042629232c11a0737b3bf1f42da74195fd8f9497
brjatoba92 <brjatoba92@gmail.com>
2023-11-16 15:23:10 -0300

    input action with useState for save user in the index.js file of the Home Screen

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,7 +1,10 @@
+import { useState } from 'react'
 import './index.css';
 import {Button, Input } from '../../components'
 
 export const HomeScreen = () =>{
+    const [username, setUsername] = useState(""); 
+    
    
     return(
         <div className="home-screen-container">
@@ -10,7 +13,12 @@ export const HomeScreen = () =>{
                 <h1 className='home-screen-title'> Sua lista de supermercado mais fácil do que nunca </h1>
                 <h3 className='home-screen-subtitle'>Ajudamos você a organizar sua lista de compras de forma descomplicada.</h3>
                 <h3 className='home-screen-subtitle-description'>Digite abaixo seu usuário para ter acesso a sua lista de compras:</h3>
-                <Input label="Username" placeholder='usuario23'/>
+                <Input 
+                    onChange={(text) => setUsername(text)} //toda vez que mudar faz o text
+                    value={username} //valor do input que esta sendo salvo no useState para sempre manter no input
+                    label="Username" 
+                    placeholder='usuario23'
+                />
                 <div className='home-screen-button-container'>
                     <Button>Continuar</Button>
                 </div>
