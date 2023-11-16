271c65db8d7a219e524d56010e13e3d83f868920
brjatoba92 <brjatoba92@gmail.com>
2023-11-16 15:02:01 -0300

    input created with label and index.js creation for put the components together

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,14 +1,17 @@
 import './index.css';
-import {Button } from '../../components/Button'
+import {Button, Input } from '../../components'
 
 export const HomeScreen = () =>{
+   
     return(
         <div className="home-screen-container">
             <div className='home-screen-content-container'>
                 <img className='shopping-bag-image' src='/images/shopping-bag.svg'  alt='bag'/>
                 <h1 className='home-screen-title'> Sua lista de supermercado mais fácil do que nunca </h1>
                 <h3 className='home-screen-subtitle'>Ajudamos você a organizar sua lista de compras de forma descomplicada.</h3>
-                <div className='home-screen-form-container'>
+                <h3 className='home-screen-subtitle-description'>Digite abaixo seu usuário para ter acesso a sua lista de compras:</h3>
+                <Input label="Username" placeholder='usuario23'/>
+                <div className='home-screen-button-container'>
                     <Button>Continuar</Button>
                 </div>
             </div>
