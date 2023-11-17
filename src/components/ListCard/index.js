323cf465fdc3df0cf2b663bf895476894bc3083f
brjatoba92 <brjatoba92@gmail.com>
2023-11-17 18:35:56 -0300

    list items development - 3th commit - checkbox add in list items

--- src/components/ListCard/index.js
+++ src/components/ListCard/index.js
@@ -1,7 +1,18 @@
 import "./index.css";
 
 export const ListCard = (props) => {
-    return <div className="list-card-container">
-        <span>{props.item.name}</span>
-    </div>;
+    const { item } = props; //desconstrução do objeto via props
+    
+    return (
+        <div className="list-card-container">
+            <img
+                className="checkbox"
+                src={`/images/${item?.checked ? "checked.svg" : "unchecked.svg"}`}
+                alt="checked-item"
+            
+            />
+            <span>{item.name}</span>
+
+        </div>
+    )
 };
