31a0b635b946aa09b881e6c25b8a750678edeb00
brjatoba92 <brjatoba92@gmail.com>
2023-11-20 15:39:34 -0300

    Modal creation - 2th commit- Modal Component: style and structure

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -2,6 +2,13 @@ import './index.css'
 
 export const Modal = () => { //Screen List
     return(
-        <div className='modal'></div>
+        <div className='modal'>
+            <div className='modal-content'>
+                <div className='modal-header'>
+                    <h1>Add new items</h1>
+                    <button className='modal-close-button'/>
+                </div>
+            </div>
+        </div>
     )
 }
