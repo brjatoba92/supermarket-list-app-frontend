a6160258fd2f1d061e257481161089cb69b23380
brjatoba92 <brjatoba92@gmail.com>
2023-11-20 16:33:34 -0300

    Modal creation - 4th commit- Modal and Input Components: Add modal inputs with name and quantity useStates // add type propriety

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,6 +1,12 @@
+import {useState} from 'react'
 import './index.css'
+import { Input } from '../Input'
+import { Button } from  '../Button'
 
 export const Modal = ({ onClose }) => { //Screen List
+    const [name, setName] = useState('')
+    const [quantity, setQuantity] = useState(0)
+
     return(
         <div className='modal'>
             <div className='modal-content'>
@@ -8,6 +14,23 @@ export const Modal = ({ onClose }) => { //Screen List
                     <h1>Add new items</h1>
                     <button onClick={onClose} className='modal-close-button'/>
                 </div>
+                <Input 
+                    onChange={(text) => setName(text)} //toda vez que mudar faz o text
+                    value={name} //valor do input que esta sendo salvo no useState para sempre manter no input
+                    label="Name" 
+                    placeholder='Ex: Açai'
+                />
+                <Input 
+                    onChange={(text) => setQuantity(text)} //toda vez que mudar faz o text
+                    value={quantity} //valor do input que esta sendo salvo no useState para sempre manter no input
+                    label="Quantity"
+                    type="number"
+                />
+                <div className='modal-spacer'/>
+                <Button>Adicionar</Button>
+                
+                
+                
             </div>
         </div>
     )
