016cd0001f11f368cf387c46bd5f1966b6ae5925
brjatoba92 <brjatoba92@gmail.com>
2023-11-20 15:51:38 -0300

    Modal creation - 3th commit- Modal Component: Add Close Modal Action in the Header Button

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,12 +1,12 @@
 import './index.css'
 
-export const Modal = () => { //Screen List
+export const Modal = ({ onClose }) => { //Screen List
     return(
         <div className='modal'>
             <div className='modal-content'>
                 <div className='modal-header'>
                     <h1>Add new items</h1>
-                    <button className='modal-close-button'/>
+                    <button onClick={onClose} className='modal-close-button'/>
                 </div>
             </div>
         </div>
