bc51bf8c8d4d3d62652bee7c0f5fba18c6275ee1
brjatoba92 <brjatoba92@gmail.com>
2023-11-21 16:35:13 -0300

    Delete Item in Modal - 2th commit - Created Button main and Button outline

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,8 +1,13 @@
 import './index.css'
 
-export const Button = ({ children, onClick }) => {
+export const Button = ({ children, onClick, variant }) => {
     return (
-        <button onClick={onClick} className='button-container'>
+        <button 
+            onClick={onClick} 
+            className={`button-container ${
+                variant === 'outline' ? 'outline' : 'main'
+            }`}
+        >
             {children}
         </button>
     )
