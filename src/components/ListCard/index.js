5f6930fea0705073d7377dc5208395c87ba196e8
brjatoba92 <brjatoba92@gmail.com>
2023-11-22 12:34:07 -0300

    Add checked function list items - finished

--- src/components/ListCard/index.js
+++ src/components/ListCard/index.js
@@ -1,21 +1,29 @@
 import "./index.css";
 
+
 export const ListCard = (props) => {
-    const { item, onClick } = props; //desconstrução do objeto via props
-    
+    const { item, onClick, onCheckItem } = props; //desconstrução do objeto via props
     return (
         <div className="list-card-container">
-            <img
-                className="checkbox"
-                src={`/images/${item?.checked ? "checked.svg" : "unchecked.svg"}`}
-                alt="checked-item"
-            
-            />
-            <div className="list-card-text-container" onClick={() => onClick(item)}>
+           <div className="checkbox">
+                <input
+                    onChange={()=>onCheckItem(item)}
+                    checked={item.checked}
+                    id="checkbox"
+                    type="checkbox"
+                />
+                <label htmlFor="checkbox"/>
+           </div>
+            <div className="list-card-text-container">
                 <span className="list-card-title">{item?.name}</span>
                 <span className="list-card-subtitle">{item?.quantity} unidades</span>
             </div>
-            <img className="arrow-icon" src="/images/arrow.svg" alt="arrow" />
+            <div onClick={() => onClick(item)} className="arrow-icon-container">
+                <img
+                    className="arrow-icon" 
+                    src="/images/arrow.svg" 
+                    alt="arrow" />
+            </div>
         </div>
-    )
+    );
 };
