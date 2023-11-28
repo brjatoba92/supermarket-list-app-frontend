7e830251422122ee695f6fe74c7299f09696efc5
brjatoba92 <brjatoba92@gmail.com>
2023-11-28 12:12:16 -0300

    feat - implementation Atomic Design - 4th Commit - Button and Loader Components Implementeds

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -1,1 +1,3 @@
 export * from './Text'
+export * from './Button'
+export * from './Loader'
--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,5 +1,6 @@
 export const colors = {
   primary: '#7785DB',
   white: '#ffffff',
-  black: '#000000'
+  black: '#000000',
+  light: '#F2F4FF'
 }
