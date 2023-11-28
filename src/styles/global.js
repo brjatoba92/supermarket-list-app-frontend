9b8998f63a282f5db84206cde9ed3ebd1502026a
brjatoba92 <brjatoba92@gmail.com>
2023-11-27 21:59:07 -0300

    feat - Implementation of Style Components - 3th Commit - Global Style Implemented

---
+++ src/styles/global.js
@@ -0,0 +1,63 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+    @font-face {
+        font-family: "Avenir Next";
+        src: url("../public/fonts/AvenirNext-Bold.otf");
+        font-weight: bold;
+    }
+
+    @font-face {
+        font-family: "Avenir Next";
+        src: url("../public/fonts/AvenirNext-Regular.otf");
+    }
+
+    @font-face {
+        font-family:"Avenir Next";
+        src: url("../public/fonts/AvenirNext-It.otf");
+        font-style: italic;
+    }
+
+    @font-face {
+        font-family:"Avenir Next";
+        src: url("../public/fonts/AvenirNext-Medium.ttf");
+        font-weight: 500;
+    }
+    body {
+        margin: 0;
+        font-family: "Avenir Next";
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+    code {
+        font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+        monospace;
+    }
+
+    ::-webkit-scrollbar{
+        visibility: hidden;
+        width: 0px;
+    }
+
+    h1{
+        font-weight: bold;
+        font-size: 24px;
+        color: black;
+        line-height: 22px;
+    }
+
+    h3{
+        font-size: 16px;
+        font-weight: 500;
+        margin: 0px;
+        padding: 0px;
+    }
+    @media (max-width: 420px){
+        h1{
+            font-size: 18px ;
+        }
+        h3{
+            font-size: 12px;
+        }
+    }
+`
